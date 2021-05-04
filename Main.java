e1250f284942b8b9f8359f31db535ba1ede42efe
rexjpc <56672509+rexjpc@users.noreply.github.com>
2021-05-04 15:00:37 +0800

    version1

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("---- Start ----");
+        //alg.initAndRun();
+        //---------------------------------------------------------------------------
+        Retrieve_Data.retrieve_data();
+        //---------------------------------------------------------------------------
+
+        System.out.println("---- End ----");
+
+    }
+}
