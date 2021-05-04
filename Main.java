22a744f6538951bb7a5d482009c3f1e8d45d6c20
rexjpc <rexjiang@fuji.waseda.jp>
2021-05-04 16:38:17 +0800

    CSV Writter

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
-import java.io.*;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("---- Start ----");
         //alg.initAndRun();
         //---------------------------------------------------------------------------
-        Retrieve_Data.retrieve_data();
+        ArrayList<ArrayList<Integer>> data = Retrieve_Data.retrieve_data();
         //---------------------------------------------------------------------------
+        String csvFilePath = "/Users/rexjiang/desktop/";
+        String[] csvHeaders = {"login", "select", "charge_back", "pay_pw_back",
+                "decide_votes_back", "confirm_back", "logout", "NoVoteFlipping",
+                "NoPersonalInfoLeak", "NoVotesNumChange", "ChargeAfterLogin",
+                "NoConfirmAfterSelectUntilVote", "PayAfterCharge"};
+
+        CsvWriter.writeCSV(data, csvHeaders ,csvFilePath);
+
 
         System.out.println("---- End ----");
 
